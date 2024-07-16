47a78aef5152dfb607a1d9ad8d201c722553e861
Arthur Calorio Martoneto <arthurcalorio@gmail.com>
2024-07-16 16:53:03 -0300

    Terceiro Commit - Arrumando Django

--- dentadura/dentadura/settings.py
+++ dentadura/dentadura/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e1p15(vm2d7q=or)&+g$@ocmraq98r=-5x_gkfk01f*n(n3&35
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dentadura-production.up.railway.app']
 
 
 # Application definition
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'dentadura.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'nDDKGYWuhMLmMlMHGEniFGfVxolPrgVE',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '17398',
     }
 }
+
 
 
 # Password validation
